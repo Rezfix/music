8164eb4a4936b19ee981165acee88c9067ba8042
Rezfix <44650563+Rezfix@users.noreply.github.com>
2018-11-08 22:27:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -357,19 +357,7 @@ if (message.content.startsWith(adminprefix + 'setT')) {
 }
 });
  
-client1.login(process.env.BOT_TOKEN);
-
-const Discord = require('discord.js');
-const Util = require('discord.js');
-const getYoutubeID = require('get-youtube-id');
-const fetchVideoInfo = require('youtube-info');
-const YouTube = require('simple-youtube-api');
-const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
-const queue = new Map();
-const ytdl = require('ytdl-core');
-const fs = require('fs');
-const gif = require("gif-search");
-const client2 = new Discord.Client({disableEveryone: true});
+client.login(process.env.BOT_TOKEN);
  
 const prefix = "2";
 /////////////////////////
