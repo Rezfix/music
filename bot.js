209f008eac376bdd083d550bd8b383f79564ed37
Rezfix <44650563+Rezfix@users.noreply.github.com>
2018-11-08 21:13:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,29 +1,14 @@
-const Discord = require("discord.js");
-const ytdl = require("ytdl-core");
-const { Client, Util } = require('discord.js');
-const fs = require('fs');
+const Discord = require('discord.js');
+const Util = require('discord.js');
 const getYoutubeID = require('get-youtube-id');
-const moment = require('moment');
-const db = require('quick.db');
-const client = new Discord.Client();  
-const giphy = require('giphy-api')();    
-const googl = require('goo.gl');  
-const translate = require('google-translate-api');  
-const UserBlocked = new Set();
-const jimp = require('jimp');  
-const math = require('math-expression-evaluator');
-const stripIndents = require('common-tags').stripIndents;
-const figlet = require('figlet');
-const google = require('google-it');
-const queue = new Map();
-const zalgo = require('zalgolize');  
 const fetchVideoInfo = require('youtube-info');
 const YouTube = require('simple-youtube-api');
 const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
-const sql = require("sqlite");
- const dateFormat = require('dateformat');
- const pretty = require('pretty-ms')
-const sWlc = {}
+const queue = new Map();
+const ytdl = require('ytdl-core');
+const fs = require('fs');
+const gif = require("gif-search");
+const client = new Discord.Client({disableEveryone: true});
  
 /*
 البكجآت
