4c3ca9488ec7976be4a88fc11431b493a6330717
Rezfix <44650563+Rezfix@users.noreply.github.com>
2019-02-23 16:16:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,50 +12,6 @@ client.on('ready', () => {
   console.log('╚[════════════]╝')
 });
 
-   client.on('message', message => {
-    const prefix = '*'
-var args = message.content.split(" ").slice(1);    
-if(message.content.startsWith(prefix + 'id')) {
-var year = message.author.createdAt.getFullYear()
-var month = message.author.createdAt.getMonth()
-var day = message.author.createdAt.getDate()
-var men = message.mentions.users.first();  
-let args = message.content.split(' ').slice(1).join(' ');
-if (args == '') {
-var z = message.author;
-}else {
-var z = message.mentions.users.first();
-}
-
-let d = z.createdAt;          
-let n = d.toLocaleString();   
-let x;                       
-let y;                        
-
-if (z.presence.game !== null) {
-y = `${z.presence.game.name}`;
-} else {
-y = "Not Playing....";
-}
-let embed = new Discord.RichEmbed()
-.setColor("#502faf")
-.addField('Name :',`**<@` + `${z.id}` + `>**`, true)
-.addField('ID :', "**"+ `${z.id}` +"**",true)
-.addField('Playing :','**'+y+'**' , true)
-.addField('Discrim :',"**#" +  `${z.discriminator}**`,true)
-.addField('**Created At**', message.author.createdAt.toLocaleString())
-.addField("**Joined At**", message.member.joinedAt.toLocaleString())    
-
-.setThumbnail(`${z.avatarURL}`)
-.setFooter(message.author.username, message.author.avatarURL)
-
-message.channel.send({embed});
-    if (!message) return message.reply('**ضع المينشان بشكل صحيح  ❌ **').catch(console.error);
-
-}
-
-});
-
 client.on("message", message => {
     if (message.content.startsWith(prefix + "obc")) { ///@» MHSTR 🇮🇶#1119
                  if (!message.member.hasPermission("ADMINISTRATOR"))  return; //@» MHSTR 🇮🇶#1119
