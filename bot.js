cdc69364d6743eb23edbde2334773c69cbb0fc68
Rezfix <44650563+Rezfix@users.noreply.github.com>
2019-02-23 15:45:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -84,36 +84,38 @@ client.on("message", message => {
   };     /// @» MHSTR 🇮🇶#1119
   }); ///@» MHSTR 🇮🇶#1119
 
+const developers = ["547113238588817409"]
 client.on('message', message => {
-  let argresult = message.content.split(``).slice(1).join(' ');
-  if (message.content.startsWith(prefix + 'setStreaming')) {
-    if (!devs.includes(message.author.id)) return message.channel.send("<@480407581085532180> only this guy can do restart the bot so don't try again 😉.");
-    message.delete();
-    client.user.setGame(argresult, 'https://twitch.tv/DynastyShop');
-
-  } else if(message.content.startsWith(prefix + 'setWatching')) {
-      client.user.setActivity(argresult,{type: 'WATCHING'});
-
-    } else if(message.content.startsWith(prefix + 'setListening')) {
-      client.user.setActivity(argresult,{type: 'LISTENING'});
-
-    } else if(message.content.startsWith(prefix + 'setPlaying')) {
-      client.user.setActivity(argresult,{type: 'PLAYING'});
-
-    } else if(message.content.startsWith(prefix + 'setName')) {
-      client.user.setUsername(argresult);
-
-    } else if(message.content.startsWith(prefix + 'setAvatar')) {
-      client.user.setAvatar(argresult);
-
-
-    } else if(message.content.startsWith(prefix + 'setStatus')) {
-      if(!argresult) return message.channel.send('online,DND(Do not Distrub),`idle, invisible(Offline) :notes: أختر أحد الحالات');
-      client.user.setStatus(argresult);
-
-
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'setg')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+     if (message.content === (adminprefix + "leave")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(adminprefix + 'setw')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'setl')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'sets')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/dream");
+      message.channel.send(`**✅**`)
   }
-
+  if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith(adminprefix + 'setava')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
 });
 
 client.login(process.env.BOT_TOKEN);
